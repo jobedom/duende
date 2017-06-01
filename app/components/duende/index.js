85e79458a1360e6a6181861cde75f3ec61dd77a0
Joaquín Bernal <joaquin.bernal@tecnilogica.com>
2017-06-01 10:01:19 +0200

    Initial document loading and watching

--- app/components/duende/index.js
+++ app/components/duende/index.js
@@ -1,15 +1,66 @@
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+const chokidar = require('chokidar')
+
 module.exports = {
 
    data() {
       return {
-         number: 100
+         docFilePath: null,
+         title: '',
+         options: []
       }
    },
 
    methods: {
-      incNumber() {
-         ++this.number
+      loadDocument() {
+         this.title = ''
+         this.options = []
+         if (!this.docFilePath)
+            return
+         fs.readFile(this.docFilePath, 'utf8', (error, str) => {
+            if (error)
+               console.error(error)
+            else {
+               const doc = JSON.parse(str)
+               this.title = doc.title
+               _.each(doc.options, item => {
+                  const separator = (item === '-')
+                  if (separator)
+                     item = {
+                        label: ''
+                     }
+                  item.separator = separator
+                  this.options.push(item)
+               })
+            }
+         })
+      },
+
+      setupDocumentWatcher() {
+         this.watcher = chokidar.watch(this.docFilePath)
+         this.watcher.on('change', () => {
+            this.loadDocument()
+         })
+         this.watcher.on('unlink', () => {
+            console.log('UNLINK!')
+         })
+      },
+
+      removeDocumentWatcher() {
+         this.watcher.close()
       }
+   },
+
+   mounted() {
+      this.docFilePath = path.resolve(__dirname, '../../../.duende')
+      this.loadDocument()
+      this.setupDocumentWatcher()
+   },
+
+   destroyed() {
+      this.removeDocumentWatcher()
    }
 
 }
